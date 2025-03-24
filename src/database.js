24f55def2576276a98b1867fa6b453583a5e4168
Luis Márquez <141973599+luisivmaraz@users.noreply.github.com>
2025-03-23 18:06:20 -0600

    importacion de dbs.js

--- src/database.js
+++ src/database.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.CONECTION_DB, {
-    serverSelectionTimeoutMS: 30000, 
-    socketTimeoutMS: 45000, 
-})
-.then(db => console.log('Database connected'))
-.catch(err => console.error('Error connecting to MongoDB:', err));
+mongoose.connect(process.env.CONECTION_DB)
+.then(db=>console.log('Database conected'))
+.catch(err=>console.error(err))
 
-export default mongoose;
+export default mongoose
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const C = console.log.bind(console.log)
 import app from "./app.js";
+import database from "./database.js"
+
 
 app.listen(process.env.SERVER_PORT,()=>{
     C("Server on port: " + process.env.SERVER_PORT)
